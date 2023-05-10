516c566b03ba50d80429d74e5d2f892e4f3334ca
Ciwy <c.walter@afk-dev.de>
2023-05-10 15:01:58 +0200

    step2: use Bottle as app

--- src/run.py
+++ src/run.py
@@ -1,7 +1,24 @@
-from bottle import route, run, template
+#!/usr/bin/env python
+#-*- encode: utf-8 -*-
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
 
-run(host='localhost', port=8080)
+# Import Third-Party
+from bottle import run
+from bottle import Bottle
+
+
+app = Bottle()
+
+
+@app.route('/index')
+def index():
+    return '<h1>INDEX</h1>'
+
+
+@app.route('/hello/<name>')
+def hello(name):
+    return f'hello {name}'
+
+
+
+run(app, host='localhost', port=8080, reloader=True)
