106d56649f238a52018dfea916a0b365b34309a3
Ciwy <c.walter@afk-dev.de>
2023-05-10 15:19:55 +0200

    step3: wrapper around Bottle

--- src/run.py
+++ src/run.py
@@ -4,20 +4,57 @@
 
 # Import Third-Party
 from bottle import run
-from bottle import Bottle
+from bottle import Bottle, Route
 
 
-app = Bottle()
+class Backend(Bottle):
+    
+    controllers = {}
+
+    def add_route(self, abs_path, http_method, callback):
+
+        route = Route(self, abs_path, http_method, callback)
+        super().add_route(route)
+
+    def init_controller(self, controller_cls):
+        ctrl = controller_cls(self)
+         
+        self.controllers[ctrl.instance_name]  = ctrl
+
+        path_base, routes = ctrl.make_routes()
+        for http_method, rel_path, callback in routes:
+            abs_path = f'{path_base}/{rel_path}'
+            self.add_route(abs_path, http_method, callback)
 
 
-@app.route('/index')
-def index():
-    return '<h1>INDEX</h1>'
+class Controller:
+    instance_name = 'test'
+
+    def __init__(self, app):
+        self.app = app
+
+    def make_routes(self):
+        path_base = '/test'
+        routes = [
+            ("GET", "index", self.index),
+            ("GET", "second", self.second),
+            ]
+        return path_base, routes
 
 
-@app.route('/hello/<name>')
-def hello(name):
-    return f'hello {name}'
+    def index(self):
+        return 'CONTROLLER INDEX'
+
+
+    def second(self):
+        return 'second route'
+
+
+
+app = Backend()
+app.init_controller(Controller)
+
+
 
 
 
